71c06b1c82d68de18c83525de1195d421aff975b
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-08-27 21:29:09 +0100

    Fix Reactor Remote tooltip import

--- src/main/java/matteroverdrive/item/ReactorRemoteItem.java
+++ src/main/java/matteroverdrive/item/ReactorRemoteItem.java
@@ -16,6 +16,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
