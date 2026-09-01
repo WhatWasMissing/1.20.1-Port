4c24b2218f1c083ce508acf16afe8b60d05a6376
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-01 20:24:33 +0100

    Register Charging Station screen

--- src/main/java/matteroverdrive/client/ClientModEvents.java
+++ src/main/java/matteroverdrive/client/ClientModEvents.java
@@ -2,6 +2,7 @@ package matteroverdrive.client;
 
 import matteroverdrive.MatterOverdrive;
 import matteroverdrive.client.screen.DecomposerScreen;
+import matteroverdrive.client.screen.ChargingStationScreen;
 import matteroverdrive.client.screen.EnergyPipeScreen;
 import matteroverdrive.client.screen.InscriberScreen;
 import matteroverdrive.client.screen.TransporterScreen;
@@ -36,6 +37,7 @@ public final class ClientModEvents {
     public static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             MenuScreens.register(ModMenus.ENERGY_PIPE.get(), EnergyPipeScreen::new);
+            MenuScreens.register(ModMenus.CHARGING_STATION.get(), ChargingStationScreen::new);
             MenuScreens.register(ModMenus.FUSION_REACTOR_CONTROLLER.get(), FusionReactorScreen::new);
             MenuScreens.register(ModMenus.TRANSPORTER.get(), TransporterScreen::new);
             MenuScreens.register(ModMenus.INSCRIBER.get(), InscriberScreen::new);
