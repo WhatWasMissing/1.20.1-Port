eee8d563ec6af3b17b3f588f16fd0939c63529a1
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-01 22:36:53 +0100

    Register Contract Market and Star Map menus

--- src/main/java/matteroverdrive/registry/ModMenus.java
+++ src/main/java/matteroverdrive/registry/ModMenus.java
@@ -4,6 +4,8 @@ import matteroverdrive.MatterOverdrive;
 import matteroverdrive.menu.DecomposerMenu;
 import matteroverdrive.menu.AndroidStationMenu;
 import matteroverdrive.menu.NetworkRouterMenu;
+import matteroverdrive.menu.ContractMarketMenu;
+import matteroverdrive.menu.StarMapMenu;
 import matteroverdrive.menu.ChargingStationMenu;
 import matteroverdrive.menu.EnergyPipeMenu;
 import matteroverdrive.menu.InscriberMenu;
@@ -26,6 +28,8 @@ import net.minecraftforge.registries.RegistryObject;
 
 public final class ModMenus {
     public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, MatterOverdrive.MOD_ID);
+    public static final RegistryObject<MenuType<ContractMarketMenu>> CONTRACT_MARKET = MENUS.register("contract_market", () -> IForgeMenuType.create(ContractMarketMenu::new));
+    public static final RegistryObject<MenuType<StarMapMenu>> STAR_MAP = MENUS.register("star_map", () -> IForgeMenuType.create(StarMapMenu::new));
     public static final RegistryObject<MenuType<NetworkRouterMenu>> NETWORK_ROUTER = MENUS.register("network_router", () -> IForgeMenuType.create(NetworkRouterMenu::new));
     public static final RegistryObject<MenuType<AndroidStationMenu>> ANDROID_STATION = MENUS.register("android_station", () -> IForgeMenuType.create(AndroidStationMenu::new));
     public static final RegistryObject<MenuType<ChargingStationMenu>> CHARGING_STATION = MENUS.register("charging_station", () -> IForgeMenuType.create(ChargingStationMenu::new));
