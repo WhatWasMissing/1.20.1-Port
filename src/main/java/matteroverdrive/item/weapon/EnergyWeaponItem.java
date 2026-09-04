8ec142d0d8e5e8e1b01c5b58de3c1a6dd4132758
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-04 12:23:47 +0100

    Restore third-person weapon aiming pose

--- src/main/java/matteroverdrive/item/weapon/EnergyWeaponItem.java
+++ src/main/java/matteroverdrive/item/weapon/EnergyWeaponItem.java
@@ -580,7 +580,7 @@ public class EnergyWeaponItem extends Item {
 
     @Override
     public UseAnim getUseAnimation(ItemStack stack) {
-        return UseAnim.NONE;
+        return type == WeaponType.PHASER ? UseAnim.NONE : UseAnim.BOW;
     }
 
     @Override
