9f07e8423e2048dc38ae2967a52171a73f8b719c
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-05 22:23:05 +0100

    Fix Heightmap import for modern structure placement

--- src/main/java/matteroverdrive/worldgen/ModernizedStructureFeature.java
+++ src/main/java/matteroverdrive/worldgen/ModernizedStructureFeature.java
@@ -2,9 +2,9 @@ package matteroverdrive.worldgen;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Heightmap;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
