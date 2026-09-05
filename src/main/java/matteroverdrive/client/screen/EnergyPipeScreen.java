092f22461ba833ff60f52284b605fe59b7c158fe
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-05 16:53:36 +0100

    Improve Energy Pipe status GUI

--- src/main/java/matteroverdrive/client/screen/EnergyPipeScreen.java
+++ src/main/java/matteroverdrive/client/screen/EnergyPipeScreen.java
@@ -9,7 +9,7 @@ import net.minecraft.world.entity.player.Inventory;
 public class EnergyPipeScreen extends AbstractContainerScreen<EnergyPipeMenu> {
     public EnergyPipeScreen(EnergyPipeMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
-        imageWidth = 176;
+        imageWidth = 260;
         imageHeight = 169;
         inventoryLabelY = 72;
     }
@@ -23,14 +23,14 @@ public class EnergyPipeScreen extends AbstractContainerScreen<EnergyPipeMenu> {
 
     @Override
     protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
-        int x = leftPos;
-        int y = topPos;
+        int x = leftPos, y = topPos;
         MachineScreenStyle.drawFrame(graphics, x, y, imageWidth, imageHeight, inventoryLabelY,
                 MachineScreenStyle.CYAN);
         MachineScreenStyle.drawSection(graphics, x + 17, y + 29, 142, 18);
         MachineScreenStyle.drawHorizontalBar(graphics, x + 18, y + 42, 140, 4,
                 menu.stored(), menu.capacity(), MachineScreenStyle.CYAN);
         MachineScreenStyle.drawDebugPanel(graphics, x + 17, y + 49, 142, 18);
+        MachineScreenStyle.drawSection(graphics, x + 170, y + 25, 81, 42);
     }
 
     @Override
@@ -42,7 +42,16 @@ public class EnergyPipeScreen extends AbstractContainerScreen<EnergyPipeMenu> {
                 20, 52, MachineScreenStyle.DEBUG, false);
         graphics.drawString(font, "Relay: all six sides",
                 20, 61, MachineScreenStyle.MUTED, false);
+
+        graphics.drawString(font, "ENERGY RELAY", 177, 32, MachineScreenStyle.CYAN, false);
+        graphics.drawString(font, percent() + "% full", 177, 44, MachineScreenStyle.TEXT, false);
+        graphics.drawString(font, "Out " + menu.output() + " FE/t", 177, 56,
+                menu.output() > 0 ? MachineScreenStyle.GREEN : MachineScreenStyle.MUTED, false);
         graphics.drawString(font, playerInventoryTitle, 8, inventoryLabelY,
                 MachineScreenStyle.MUTED, false);
+    }
+
+    private int percent() {
+        return menu.capacity() <= 0 ? 0 : Math.min(100, menu.stored() * 100 / menu.capacity());
     }
 }
