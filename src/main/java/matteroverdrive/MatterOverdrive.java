3f4e909029412467f4c63c8a563e4581ea714f82
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-06 17:56:41 +0100

    Register native structure types

--- src/main/java/matteroverdrive/MatterOverdrive.java
+++ src/main/java/matteroverdrive/MatterOverdrive.java
@@ -10,6 +10,7 @@ import matteroverdrive.registry.ModExtraBlockEntities;
 import matteroverdrive.registry.ModFeatures;
 import matteroverdrive.registry.ModItems;
 import matteroverdrive.registry.ModMenus;
+import matteroverdrive.registry.ModStructures;
 import matteroverdrive.network.ModNetwork;
 import matteroverdrive.registry.ModSounds;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -36,6 +37,8 @@ public final class MatterOverdrive {
         ModExtraBlockEntities.BLOCK_ENTITIES.register(modBus);
         ModMenus.MENUS.register(modBus);
         ModFeatures.FEATURES.register(modBus);
+        ModStructures.STRUCTURE_TYPES.register(modBus);
+        ModStructures.STRUCTURE_PIECES.register(modBus);
         modBus.addListener(ModCapabilities::register);
         modBus.addListener(this::commonSetup);
     }
@@ -43,6 +46,6 @@ public final class MatterOverdrive {
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(ModNetwork::register);
         LOGGER.info("M1 VERIFY: Matter Overdrive registry shell initialized - blocks={}, blockItems={}, standaloneItems={}, sounds={}", ModBlocks.all().size(), ModItems.BLOCK_ITEMS.size(), ModItems.STANDALONE_ITEMS.size(), ModSounds.all().size());
-        LOGGER.info("M2 VERIFY: machine foundation initialized - blockEntities=18, menus=15, gunSystem=enabled, weaponStation=enabled, energyPipe=enabled, fusionReactor=enabled, transporter=enabled, inscriber=enabled, decomposer=enabled, recycler=enabled, microwave=enabled, spacetimeAccelerator=enabled, handheldMatterTools=enabled, androidAbilities=enabled, documentationItems=enabled, systemGuide=enabled, analyzer=enabled, replicator=enabled, patternStorage=enabled, patternMonitor=enabled, solarPanel=enabled, tritaniumCrate=enabled, networkPipe=enabled, matterPipe=enabled, creativeBattery=enabled, legacyEntities=enabled, holoSign=enabled, securityProtocol=enabled, legacyWorldFeatures=enabled");
+        LOGGER.info("M2 VERIFY: machine foundation initialized - blockEntities=18, menus=15, gunSystem=enabled, weaponStation=enabled, energyPipe=enabled, fusionReactor=enabled, transporter=enabled, inscriber=enabled, decomposer=enabled, recycler=enabled, microwave=enabled, spacetimeAccelerator=enabled, handheldMatterTools=enabled, androidAbilities=enabled, documentationItems=enabled, systemGuide=enabled, analyzer=enabled, replicator=enabled, patternStorage=enabled, patternMonitor=enabled, solarPanel=enabled, tritaniumCrate=enabled, networkPipe=enabled, matterPipe=enabled, creativeBattery=enabled, legacyEntities=enabled, holoSign=enabled, securityProtocol=enabled, nativeStructures=enabled");
     }
 }
