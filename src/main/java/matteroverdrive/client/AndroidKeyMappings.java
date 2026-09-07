c98c459a6b9afdaac3885424d9126160582738b4
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-07 08:51:57 +0100

    Add Android loadout key binding

--- src/main/java/matteroverdrive/client/AndroidKeyMappings.java
+++ src/main/java/matteroverdrive/client/AndroidKeyMappings.java
@@ -11,32 +11,18 @@ import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(modid = MatterOverdrive.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class AndroidKeyMappings {
-    public static final KeyMapping CYCLE_ABILITY = new KeyMapping(
-            "key.matteroverdrive.android_cycle_ability",
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_V,
-            "key.categories.matteroverdrive"
-    );
-    public static final KeyMapping OPEN_SKILL_TREE = new KeyMapping(
-            "key.matteroverdrive.android_skill_tree",
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_K,
-            "key.categories.matteroverdrive"
-    );
-    public static final KeyMapping ACTIVATE_ABILITY = new KeyMapping(
-            "key.matteroverdrive.android_activate_ability",
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_B,
-            "key.categories.matteroverdrive"
-    );
+    public static final KeyMapping CYCLE_ABILITY = new KeyMapping("key.matteroverdrive.android_cycle_ability", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, "key.categories.matteroverdrive");
+    public static final KeyMapping OPEN_SKILL_TREE = new KeyMapping("key.matteroverdrive.android_skill_tree", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_K, "key.categories.matteroverdrive");
+    public static final KeyMapping OPEN_LOADOUT = new KeyMapping("key.matteroverdrive.android_loadout", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_L, "key.categories.matteroverdrive");
+    public static final KeyMapping ACTIVATE_ABILITY = new KeyMapping("key.matteroverdrive.android_activate_ability", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_B, "key.categories.matteroverdrive");
 
-    private AndroidKeyMappings() {
-    }
+    private AndroidKeyMappings() {}
 
     @SubscribeEvent
     public static void register(RegisterKeyMappingsEvent event) {
         event.register(CYCLE_ABILITY);
         event.register(ACTIVATE_ABILITY);
         event.register(OPEN_SKILL_TREE);
+        event.register(OPEN_LOADOUT);
     }
 }
