e85155fc3c80e6e8f4080418095163482cce9d42
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-07 13:47:43 +0100

    Route Android ultimate activation

--- src/main/java/matteroverdrive/network/AndroidAbilityPacket.java
+++ src/main/java/matteroverdrive/network/AndroidAbilityPacket.java
@@ -1,6 +1,7 @@
 package matteroverdrive.network;
 
 import matteroverdrive.android.AndroidAbilities;
+import matteroverdrive.android.AndroidUltimates;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkEvent;
@@ -20,7 +21,10 @@ public record AndroidAbilityPacket(int action) {
         NetworkEvent.Context context = supplier.get();
         ServerPlayer sender = context.getSender();
         if (sender != null) {
-            context.enqueueWork(() -> AndroidAbilities.handleAction(sender, packet.action));
+            context.enqueueWork(() -> {
+                if (packet.action == AndroidUltimates.ACTION_ACTIVATE_ULTIMATE) AndroidUltimates.activate(sender);
+                else AndroidAbilities.handleAction(sender, packet.action);
+            });
         }
         context.setPacketHandled(true);
     }
