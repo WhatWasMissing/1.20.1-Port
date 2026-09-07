26bfb6f49ebc2b157739035960519983a76b1c11
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-07 14:03:07 +0100

    Open three-class Android loadout screen

--- src/main/java/matteroverdrive/client/AndroidClientInput.java
+++ src/main/java/matteroverdrive/client/AndroidClientInput.java
@@ -3,7 +3,7 @@ package matteroverdrive.client;
 import matteroverdrive.MatterOverdrive;
 import matteroverdrive.android.AndroidAbilities;
 import matteroverdrive.android.AndroidUltimates;
-import matteroverdrive.client.screen.AndroidLoadoutScreen;
+import matteroverdrive.client.screen.AndroidClassLoadoutScreen;
 import matteroverdrive.client.screen.AndroidSkillTreeScreen;
 import matteroverdrive.network.AndroidAbilityPacket;
 import matteroverdrive.network.ModNetwork;
@@ -23,7 +23,7 @@ public final class AndroidClientInput {
         Minecraft minecraft = Minecraft.getInstance();
         if (minecraft.player == null || minecraft.screen != null || !AndroidClientState.isActive()) return;
         while (AndroidKeyMappings.OPEN_SKILL_TREE.consumeClick()) minecraft.setScreen(new AndroidSkillTreeScreen());
-        while (AndroidKeyMappings.OPEN_LOADOUT.consumeClick()) minecraft.setScreen(new AndroidLoadoutScreen());
+        while (AndroidKeyMappings.OPEN_LOADOUT.consumeClick()) minecraft.setScreen(new AndroidClassLoadoutScreen());
         while (AndroidKeyMappings.CYCLE_ABILITY.consumeClick()) ModNetwork.CHANNEL.sendToServer(new AndroidAbilityPacket(AndroidAbilities.ACTION_CYCLE));
         while (AndroidKeyMappings.ACTIVATE_ABILITY.consumeClick()) ModNetwork.CHANNEL.sendToServer(new AndroidAbilityPacket(AndroidAbilities.ACTION_ACTIVATE));
         while (AndroidKeyMappings.ACTIVATE_ULTIMATE.consumeClick()) ModNetwork.CHANNEL.sendToServer(new AndroidAbilityPacket(AndroidUltimates.ACTION_ACTIVATE_ULTIMATE));
