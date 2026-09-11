e4d16901033b91258608f6f9a0fc3c62b72a329d
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-11 16:47:07 +0100

    Add first-world briefing packet

---
+++ src/main/java/matteroverdrive/network/WelcomeBriefingPacket.java
@@ -0,0 +1,22 @@
+package matteroverdrive.network;
+
+import matteroverdrive.client.ClientWelcomeOpener;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.function.Supplier;
+
+/** Opens the client field briefing after the player's first login to a world. */
+public record WelcomeBriefingPacket() {
+    public static void encode(WelcomeBriefingPacket packet, FriendlyByteBuf buffer) { }
+    public static WelcomeBriefingPacket decode(FriendlyByteBuf buffer) { return new WelcomeBriefingPacket(); }
+
+    public static void handle(WelcomeBriefingPacket packet, Supplier<NetworkEvent.Context> supplier) {
+        NetworkEvent.Context context = supplier.get();
+        context.enqueueWork(() -> DistExecutor.unsafeRunWhenOn(Dist.CLIENT,
+                () -> ClientWelcomeOpener::openFirstWorldBriefing));
+        context.setPacketHandled(true);
+    }
+}
