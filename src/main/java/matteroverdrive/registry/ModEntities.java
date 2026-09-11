82324c80cbdc0095bed802bdb54a120b38e197c5
MVQ1303 <296129285+WhatWasMissing@users.noreply.github.com>
2026-09-11 16:25:49 +0100

    Register facility NPCs and new Android enemies

--- src/main/java/matteroverdrive/registry/ModEntities.java
+++ src/main/java/matteroverdrive/registry/ModEntities.java
@@ -1,14 +1,18 @@
 package matteroverdrive.registry;
 
 import matteroverdrive.MatterOverdrive;
+import matteroverdrive.entity.DefectorAndroidEntity;
 import matteroverdrive.entity.DroneEntity;
+import matteroverdrive.entity.FacilityResearcherEntity;
 import matteroverdrive.entity.FailedChickenEntity;
 import matteroverdrive.entity.FailedCowEntity;
 import matteroverdrive.entity.FailedPigEntity;
 import matteroverdrive.entity.FailedSheepEntity;
 import matteroverdrive.entity.MadScientistEntity;
 import matteroverdrive.entity.MutantScientistEntity;
+import matteroverdrive.entity.OrpheusSecurityEntity;
 import matteroverdrive.entity.RangedRogueAndroidEntity;
+import matteroverdrive.entity.ResonantAndroidEntity;
 import matteroverdrive.entity.RogueAndroidEntity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
@@ -18,8 +22,15 @@ import net.minecraftforge.registries.RegistryObject;
 
 public final class ModEntities {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MatterOverdrive.MOD_ID);
+
     public static final RegistryObject<EntityType<RogueAndroidEntity>> ROGUE_ANDROID = ENTITY_TYPES.register("rogue_android", () -> EntityType.Builder.of(RogueAndroidEntity::new, MobCategory.MONSTER).sized(0.6F,1.8F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":rogue_android"));
     public static final RegistryObject<EntityType<RangedRogueAndroidEntity>> RANGED_ROGUE_ANDROID = ENTITY_TYPES.register("ranged_rogue_android", () -> EntityType.Builder.of(RangedRogueAndroidEntity::new, MobCategory.MONSTER).sized(0.6F,1.8F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":ranged_rogue_android"));
+
+    public static final RegistryObject<EntityType<DefectorAndroidEntity>> DEFECTOR_ANDROID = ENTITY_TYPES.register("defector_android", () -> EntityType.Builder.of(DefectorAndroidEntity::new, MobCategory.CREATURE).sized(0.6F,1.8F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":defector_android"));
+    public static final RegistryObject<EntityType<OrpheusSecurityEntity>> ORPHEUS_SECURITY = ENTITY_TYPES.register("orpheus_security", () -> EntityType.Builder.of(OrpheusSecurityEntity::new, MobCategory.MONSTER).sized(0.6F,1.8F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":orpheus_security"));
+    public static final RegistryObject<EntityType<ResonantAndroidEntity>> RESONANT_ANDROID = ENTITY_TYPES.register("resonant_android", () -> EntityType.Builder.of(ResonantAndroidEntity::new, MobCategory.MONSTER).sized(0.6F,1.8F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":resonant_android"));
+    public static final RegistryObject<EntityType<FacilityResearcherEntity>> FACILITY_RESEARCHER = ENTITY_TYPES.register("facility_researcher", () -> EntityType.Builder.of(FacilityResearcherEntity::new, MobCategory.CREATURE).sized(0.6F,1.95F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":facility_researcher"));
+
     public static final RegistryObject<EntityType<DroneEntity>> DRONE = ENTITY_TYPES.register("drone", () -> EntityType.Builder.of(DroneEntity::new, MobCategory.MONSTER).sized(0.5F,0.5F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":drone"));
     public static final RegistryObject<EntityType<MutantScientistEntity>> MUTANT_SCIENTIST = ENTITY_TYPES.register("mutant_scientist", () -> EntityType.Builder.of(MutantScientistEntity::new, MobCategory.MONSTER).sized(1.0F,2.3F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":mutant_scientist"));
     public static final RegistryObject<EntityType<MadScientistEntity>> MAD_SCIENTIST = ENTITY_TYPES.register("mad_scientist", () -> EntityType.Builder.of(MadScientistEntity::new, MobCategory.CREATURE).sized(0.6F,1.95F).clientTrackingRange(10).build(MatterOverdrive.MOD_ID+":mad_scientist"));
@@ -27,5 +38,6 @@ public final class ModEntities {
     public static final RegistryObject<EntityType<FailedPigEntity>> FAILED_PIG = ENTITY_TYPES.register("failed_pig", () -> EntityType.Builder.of(FailedPigEntity::new, MobCategory.CREATURE).sized(0.9F,0.9F).clientTrackingRange(8).build(MatterOverdrive.MOD_ID+":failed_pig"));
     public static final RegistryObject<EntityType<FailedSheepEntity>> FAILED_SHEEP = ENTITY_TYPES.register("failed_sheep", () -> EntityType.Builder.of(FailedSheepEntity::new, MobCategory.CREATURE).sized(0.9F,1.3F).clientTrackingRange(8).build(MatterOverdrive.MOD_ID+":failed_sheep"));
     public static final RegistryObject<EntityType<FailedChickenEntity>> FAILED_CHICKEN = ENTITY_TYPES.register("failed_chicken", () -> EntityType.Builder.of(FailedChickenEntity::new, MobCategory.CREATURE).sized(0.4F,0.7F).clientTrackingRange(8).build(MatterOverdrive.MOD_ID+":failed_chicken"));
+
     private ModEntities() {}
 }
